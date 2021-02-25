9934f27045709a789f88ba892b87a642e5b3d933
Mari Orbak <79453517+maorbak@users.noreply.github.com>
2021-02-25 11:52:51 +0100

    Create SSID with limited API key

--- code/ssid_toggle.py
+++ code/ssid_toggle.py
@@ -4,8 +4,8 @@ import requests
 
 # ****** START EDIT THIS ******
 
-api_key = "24faa954e9ae4c856fb4c96798e177aa1816d760"  # Replace with your API key
-network_id = "L_646829496481104046"  # Replace with your Network ID
+api_key = "6bec40cf957de430a6f1f2baa056b99a4fac9ea0"  # Replace with your API key
+network_id = "L_646829496481105433"  # Replace with your Network ID
 ssid_number = "1"  # Replace with your ssid number to edit
 
 # ****** STOP EDIT THIS  ******
@@ -16,7 +16,7 @@ url = "https://api.meraki.com/api/v0/networks/{}/ssids/{}".format(
 
 headers = {"X-Cisco-Meraki-API-Key": api_key}
 payload = {
-    "name": "Wifi Name Here",
+    "name": "Mari's Wifi",
     "enabled": "true",
 }
 
